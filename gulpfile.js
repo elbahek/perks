a9f1d1aa5521c6da66083dbd58ce2c17a63a5a20
Ivan Abdulin <ivan.abdulin@gmail.com>
2015-12-21 19:32:59 +0200

    Setup compiling less assets with gulp.
    Concatenation.
    Sourcemaps on development.
    Minification on production.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,73 @@
 'use strict';
 
-var gulp = require('gulp');
+var path = require('path'),
+  _ = require('lodash'),
+  gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  gulpif = require('gulp-if'),
+  inject = require('gulp-inject'),
+  minify = require('gulp-minify-css'),
+  less = require('gulp-less'),
+  sourcemaps = require('gulp-sourcemaps'),
+  advancedWatch = require('gulp-watch');
 
-gulp.task('inject', function() {
-  return gulp.src('app/index.html')
-    .pipe(gulp.dest('gh-pages'));
+var config = require('./config');
+
+function transformPaths(filePath) {
+  var extension = path.extname(filePath).replace('.', '').toLowerCase();
+  var fileName = path.basename(filePath);
+  var localPathPart = config.environment === ENV_DEVELOPMENT ? '.' : '';
+  if (['js', 'css'].indexOf(extension) !== -1) {
+    filePath = localPathPart + '/' + extension + '/' + fileName;
+  }
+  if (extension === 'js') {
+    filePath = '<script type="text/javascript" src="' + filePath + '"></script>';
+  }
+  else if (extension === 'css') {
+    filePath = '<link rel="stylesheet" href="' + filePath + '">';
+  }
+
+  return filePath;
+}
+
+gulp.task('copyAppStyles', function() {
+  return gulp.src(config.assets.styles.app)
+    .pipe(gulpif(config.environment === ENV_DEVELOPMENT, sourcemaps.init()))
+    .pipe(less())
+    .pipe(gulpif(config.environment === ENV_PRODUCTION, minify()))
+    .pipe(gulpif(config.environment === ENV_PRODUCTION, concat('app.min.css')))
+    .pipe(gulpif(config.environment === ENV_DEVELOPMENT, sourcemaps.write()))
+    .pipe(gulp.dest(config.distDir + '/css'));
+    //.pipe(gulpif(config.environment === ENV_DEVELOPMENT, browserSync.stream()));
 });
+
+gulp.task('inject', [
+    'copyAppStyles'
+  ],
+  function() {
+    var files = null;
+    if (config.environment === ENV_DEVELOPMENT) {
+      var appStylesCompiled = [];
+      _.forOwn(config.assets.styles.app, function(asset) {
+        var newFile = path.basename(asset).replace('.less', '.css');
+        appStylesCompiled.push(config.distDir + '/css/' + newFile);
+      });
+      files = [].concat(
+        appStylesCompiled
+      );
+    }
+    else if (config.environment === ENV_PRODUCTION) {
+      files = [
+        config.distDir + '/css/app.min.css'
+      ];
+    }
+    var sources = gulp.src(files, {read: false});
+
+    return gulp.src(config.appDir + '/index.html')
+      .pipe(inject(sources, {transform: transformPaths}))
+      .pipe(gulp.dest(config.distDir));
+  }
+);
 
 gulp.task('serve', [
   'inject'
